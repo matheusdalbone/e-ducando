1731edbe196ac13debe4c99ffdb0b7692c18e954
matheusdalbone <matheusdalbone7@gmail.com>
2025-08-04 01:42:24 -0300

    feature: criando RatingModal e RatingSection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import HeroSection from "./components/HeroSection";
-import BenefitsSection from "./components/BenefitsSection";
-import BenefitsItem from "./components/BenefitsItem";
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <HeroSection />
-      <BenefitsSection />
-      <BenefitsItem />
     </div>
     
   );
--- frontend/src/components/base/Text/Text.jsx
+++ frontend/src/components/base/Text/Text.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import styles from "./styles.module.css"
 
-const Text = ({ font, size, color, weight, children, lineHeight, as: Component = 'p' }) => {
+const Text = ({ font, size, color, weight, children, lineHeight, opacity, as: Component = 'p' }) => {
   
   const textStyle = {
     color: color,
     fontSize: size,
     fontFamily: font,
     fontWeight: weight,
-    lineHeight: lineHeight 
+    lineHeight: lineHeight,
+    opacity: opacity
   };
 
   return (
