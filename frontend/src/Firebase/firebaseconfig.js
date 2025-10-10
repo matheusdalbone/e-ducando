64a624a4772f00223cd4c8cbf34d670a5a01a4f3
Genison Leal <87821998+GenisonLeal@users.noreply.github.com>
2025-10-10 10:07:35 -0300

    Update firebaseconfig.js

--- frontend/src/Firebase/firebaseconfig.js
+++ frontend/src/Firebase/firebaseconfig.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SEND_ERLD,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
