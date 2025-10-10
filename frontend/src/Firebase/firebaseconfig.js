0633b1c0679913bcb0e099a4372055fe93f906ed
Genison Leal <87821998+GenisonLeal@users.noreply.github.com>
2025-10-10 12:41:25 -0300

    Update firebaseconfig.js

--- frontend/src/Firebase/firebaseconfig.js
+++ frontend/src/Firebase/firebaseconfig.js
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.VITE_APP_API_KEY,
-  authDomain: process.env.VITE_APP_AUTH_DOMAIN,
-  projectId: process.env.VITE_APP_PROJECT_ID,
-  storageBucket: process.env.VITE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_APP_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_APP_APP_ID,
-  measurementId: process.env.VITE_APP_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
+  measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
